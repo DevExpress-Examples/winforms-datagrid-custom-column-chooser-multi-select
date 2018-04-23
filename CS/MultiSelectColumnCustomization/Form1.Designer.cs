792375e2d04d0cb189e81dd694cdff6f7a7ec485
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MultiSelectColumnCustomization/Form1.Designer.cs
@@ -0,0 +1,74 @@
+namespace MultiSelectColumnCustomization
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing && (components != null) )
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.gridControlOverride1 = new MultiSelectColumnCustomization.GridControlOverride();
+			this.gridViewOverride1 = new MultiSelectColumnCustomization.GridViewOverride();
+			((System.ComponentModel.ISupportInitialize)(this.gridControlOverride1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.gridViewOverride1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// gridControlOverride1
+			// 
+			this.gridControlOverride1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.gridControlOverride1.Location = new System.Drawing.Point(0, 0);
+			this.gridControlOverride1.MainView = this.gridViewOverride1;
+			this.gridControlOverride1.Name = "gridControlOverride1";
+			this.gridControlOverride1.Size = new System.Drawing.Size(537, 357);
+			this.gridControlOverride1.TabIndex = 0;
+			this.gridControlOverride1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridViewOverride1});
+			// 
+			// gridViewOverride1
+			// 
+			this.gridViewOverride1.GridControl = this.gridControlOverride1;
+			this.gridViewOverride1.Name = "gridViewOverride1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(537, 357);
+			this.Controls.Add(this.gridControlOverride1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.gridControlOverride1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.gridViewOverride1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private GridControlOverride gridControlOverride1;
+		private GridViewOverride gridViewOverride1;
+	}
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create custom column chooser allowing column multi select
+
+
+<p>Imagine that you have dozens of columns and you need to add them to your grid view from a column chooser. It's very inconvenient to drag and drop them one by one and it would be great to have a possibility of selecting them at once and sending to the grid view altogether. This example demonstrates what to do with the column chooser to achieve this behavior. Here we created a ColumnCustomizationListBox descendant with the capability to draw a check box across every column in the column chooser's list box. Also we have added a button to the bottom of column chooser. So now you can check columns you need and send them to the grid view by pressing the button.</p>
+
+<br/>
+
+
